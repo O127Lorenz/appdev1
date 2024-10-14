5ce3a0623b5b03b79c98312801e612b8c1fec9c0
LG0127 <lorenzreyes2004@gmail.com>
2024-10-14 23:23:21 +0900

    I’m done completing the section on updating the screen

--- react_quickstart/src/App.jsx
+++ react_quickstart/src/App.jsx
@@ -7,31 +7,27 @@ function App() {
 
 
   return (
-  <ShoppingList/>
+    <div>
+    <h1>Counters that update separately</h1>
+    <MyButton />
+    <MyButton />
+  </div>
   )
 
 };
-const products = [
-  { title: 'Cabbage', isFruit: false, id: 1 },
-  { title: 'Garlic', isFruit: false, id: 2 },
-  { title: 'Apple', isFruit: true, id: 3 },
-];
+function MyButton() {
+  const [count, setCount] = useState(0);
 
- function ShoppingList() {
-  const listItems = products.map(product =>
-    <li
-      key={product.id}
-      style={{
-        color: product.isFruit ? 'magenta' : 'darkgreen'
-      }}
-    >
-      {product.title}
-    </li>
-  );
+  function handleClick() {
+    setCount(count + 1);
+  }
 
   return (
-    <ul>{listItems}</ul>
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
   );
 }
+
 
 export default App
