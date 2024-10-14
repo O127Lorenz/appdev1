14de4f0b63f865c4d6e58acfd304168e5b43323a
LG0127 <lorenzreyes2004@gmail.com>
2024-10-14 23:19:40 +0900

    I’m done completing the section on rendering list

--- react_quickstart/src/App.jsx
+++ react_quickstart/src/App.jsx
@@ -5,17 +5,33 @@ import './App.css'
 
 function App() {
 
-const isLoggedIn = false;
+
   return (
-    <div>
-    {isLoggedIn ? (
-      <AdminPanel />
-    ) : (
-      <LoginForm />
-    )}
-  </div>
+  <ShoppingList/>
   )
 
 };
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+ function ShoppingList() {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  );
+
+  return (
+    <ul>{listItems}</ul>
+  );
+}
 
 export default App
