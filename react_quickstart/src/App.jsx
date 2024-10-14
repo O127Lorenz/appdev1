079c59d3865eb6c5b12826c89781fe60d6e77665
LG0127 <lorenzreyes2004@gmail.com>
2024-10-14 23:16:31 +0900

    I’m done completing the section on conditional rendering

--- react_quickstart/src/App.jsx
+++ react_quickstart/src/App.jsx
@@ -1,41 +1,21 @@
+import AdminPanel from './AdminPanel'
+import LoginForm from './LoginForm'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
-
+const isLoggedIn = false;
   return (
-  <Profile/>
+    <div>
+    {isLoggedIn ? (
+      <AdminPanel />
+    ) : (
+      <LoginForm />
+    )}
+  </div>
   )
-}
-const user = {
-  name: 'Hedy Lamarr',
-  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-  imageSize: 90,
+
 };
-
-function Profile() {
-  return (
-    <>
-      <h1>{user.name}</h1>
-      <img
-        className="avatar"
-        src={user.imageUrl}
-        alt={'Photo of ' + user.name}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize
-        }}
-      />
-    </>
-  );
-}
-
-
-
-
-
 
 export default App
